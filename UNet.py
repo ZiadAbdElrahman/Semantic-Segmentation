ebb40e9adeffcf471447d8917a7cc8e97d943e82
Ziad Abd Elrahman <iziad2000@outlook.com>
2019-11-06 09:55:36 +0200

    Update UNet.py

--- UNet.py
+++ UNet.py
@@ -1,5 +1,4 @@
 import torch.nn as nn
-import torchvision.models as models
 import torch, torch.nn.functional as F
 
 
@@ -8,10 +7,12 @@ class EcoderBlock(nn.Module):
         super(EcoderBlock, self).__init__()
         self.block = nn.Sequential(
             nn.Conv2d(in_ch, out_ch, kernel_size=(3, 3), bias=False),
+            nn.BatchNorm2d(out_ch, momentum=0.95),
             nn.ReLU(inplace=True),
             nn.Conv2d(out_ch, out_ch, kernel_size=(3, 3), bias=False),
+            nn.BatchNorm2d(out_ch, momentum=0.95),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernal_size=2, stride=2)
+            nn.MaxPool2d(kernel_size=2, stride=2)
         )
 
     def forward(self, x):
@@ -23,8 +24,10 @@ class DecoderBlock(nn.Module):
         super(DecoderBlock, self).__init__()
         self.block = nn.Sequential(
             nn.Conv2d(in_ch, 2 * out_ch, kernel_size=(3, 3), bias=False),
+            nn.BatchNorm2d(2 * out_ch, momentum=0.95),
             nn.ReLU(inplace=True),
             nn.Conv2d(2 * out_ch, 2 * out_ch, kernel_size=(3, 3), bias=False),
+            nn.BatchNorm2d(2 * out_ch, momentum=0.95),
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(2 * out_ch, out_ch, kernel_size=2, stride=2)
         )
@@ -34,46 +37,43 @@ class DecoderBlock(nn.Module):
 
 
 class UNet(nn.Module):
-    def __init__(self, number_of_block):
+    def __init__(self):
         super(UNet, self).__init__()
+        self.model_name = "Unet"
+
         self.EB1 = EcoderBlock(3, 64)
         self.EB2 = EcoderBlock(64, 128)
         self.EB3 = EcoderBlock(128, 256)
         self.EB4 = EcoderBlock(256, 512)
 
-        self.center = nn.Sequential(
-            nn.MaxPool2d(kernal_size=2, stride=2),
-            nn.Conv2d(512, 1024, kernel_size=(3, 3), bias=False),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(1024, 1024, kernel_size=(3, 3), bias=False),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
-        )
+        self.center = DecoderBlock(512, 512)
 
         self.DB4 = DecoderBlock(1024, 256)
         self.DB3 = DecoderBlock(512, 128)
         self.DB2 = DecoderBlock(256, 64)
         self.DB1 = nn.Sequential(
             nn.Conv2d(128, 64, kernel_size=(3, 3), bias=False),
+            nn.BatchNorm2d(64, momentum=0.95),
             nn.ReLU(inplace=True),
             nn.Conv2d(64, 64, kernel_size=(3, 3), bias=False),
+            nn.BatchNorm2d(64, momentum=0.95),
             nn.ReLU(inplace=True),
         )
 
-        self.final = nn.Conv2d(64, 29, kernal_size=1)
+        self.final = nn.Conv2d(64, 29, kernel_size=1)
 
-    def forword(self, x):
+    def forward(self, x):
         EB1_out = self.EB1(x)
         EB2_out = self.EB2(EB1_out)
-        EB3_out = self.EB2(EB2_out)
-        EB4_out = self.EB2(EB3_out)
+        EB3_out = self.EB3(EB2_out)
+        EB4_out = self.EB4(EB3_out)
 
         center_out = self.center(EB4_out)
 
-        DB4 = self.DB4(torch.concat((EB4_out, center_out), 1))
-        DB3 = self.DB3(torch.concat((EB3_out, DB4), 1))
-        DB2 = self.DB2(torch.concat((EB2_out, DB3), 1))
-        DB1 = self.DB1(torch.concat((EB1_out, DB2), 1))
+        DB4 = self.DB4(torch.cat([F.upsample(EB4_out, center_out.size()[2:], mode='bilinear'), center_out], 1))
+        DB3 = self.DB3(torch.cat([F.upsample(EB3_out, DB4.size()[2:], mode='bilinear'), DB4], 1))
+        DB2 = self.DB2(torch.cat([F.upsample(EB2_out, DB3.size()[2:], mode='bilinear'), DB3], 1))
+        DB1 = self.DB1(torch.cat([F.upsample(EB1_out, DB2.size()[2:], mode='bilinear'), DB2], 1))
 
         out = self.final(DB1)
-        return out
+        return F.upsample(out, x.size()[2:], mode='bilinear')

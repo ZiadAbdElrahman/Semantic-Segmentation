36c5b495fc7b8d264f6027e61af4167b6e4837c0
Ziad Abd Elrahman <iziad2000@outlook.com>
2019-11-06 09:57:12 +0200

    Update train.py

--- train.py
+++ train.py
@@ -6,11 +6,10 @@ from util import save_weights, load_weights
 
 
 class Trainer:
-    def __init__(self, args, device, encoder, decoder, training_data, val_data):
+    def __init__(self, args, device, model, training_data, val_data):
         self.args = args
         self.device = device
-        self.encoder = encoder.to(device)
-        self.decoder = decoder.to(device)
+        self.model = model.to(device)
         self.train_input, self.train_output = training_data
         self.test_input, self.test_output = val_data
 
@@ -18,11 +17,10 @@ class Trainer:
         writer = SummaryWriter()
 
         if self.args.load_weight:
-            load_weights(self.encoder, self.args.model_path + "encoder")
-            load_weights(self.decoder, self.args.model_path + "decoder")
+            load_weights(self.model, self.args.model_path  + self.model.name)
 
         for epoch in range(self.args.num_epochs):
-            params = list(self.decoder.parameters())
+            params = list(self.model.parameters())
             criterion = nn.CrossEntropyLoss()
             optimizer = torch.optim.Adam(params=params, lr=self.args.learning_rate)
 
@@ -34,7 +32,6 @@ class Trainer:
                 test_loss = self.step(criterion=criterion,
                                       data=(self.test_input, self.test_output))
 
-
             writer.add_scalars('loss', {'train': training_loss,
                                         'val': test_loss}, epoch + 1)
 
@@ -45,8 +42,7 @@ class Trainer:
             self.args.learning_rate *= 0.995
 
         if self.args.save_weight:
-            save_weights(self.encoder, self.args.model_path + "encoder")
-            save_weights(self.decoder, self.args.model_path + "decoder")
+            save_weights(self.model, self.args.model_path + self.model.name)
 
     def step(self, criterion, data, optimizer=None):
         data_inputs, data_outputs = data
@@ -60,9 +56,7 @@ class Trainer:
                 self.device)
             data_output = torch.from_numpy(data_outputs[start: start + self.args.batch_size]).long().to(self.device)
 
-            feature = self.encoder(data_input.permute(0, 3, 1, 2))
-
-            outputs = self.decoder(feature)
+            outputs = self.model(data_input.permute(0, 3, 1, 2))
 
             loss = criterion(outputs, data_output)
 

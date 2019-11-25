7e94e9f1bb37a81150014684569701b9e13f8b7c
Ziad Abd Elrahman <ziad.tawab2000@gmail.com>
2019-11-25 08:22:47 +0200

    add loss balancing

--- train.py
+++ train.py
@@ -4,19 +4,19 @@ from torch import nn
 from torch.utils.tensorboard import SummaryWriter
 from util import save_weights, load_weights
 from torch.utils.data import DataLoader
-from torchvision.transforms import CenterCrop
 from torchvision import transforms
-from PIL import Image
 from label import id_to_trainId_map_func, traindId_to_color_map_func
+import torch.nn.functional as F
 
 
 class Trainer:
-    def __init__(self, args, device, model, training_data, val_data):
+    def __init__(self, args, device, model, training_data, val_data, loss_balance):
         self.args = args
         self.device = device
         self.model = model.to(device)
         self.train_data = training_data
         self.val_data = val_data
+        self.loss_balance = loss_balance
 
     def train(self):
         writer = SummaryWriter()
@@ -26,16 +26,13 @@ class Trainer:
 
         for epoch in range(self.args.num_epochs):
             params = list(self.model.parameters())
-            criterion = nn.CrossEntropyLoss()
             optimizer = torch.optim.Adam(params=params, lr=self.args.learning_rate)
 
-            training_loss = self.step(criterion=criterion,
-                                      data=self.train_data,
+            training_loss = self.step(data=self.train_data,
                                       optimizer=optimizer)
 
             with torch.no_grad():
-                test_loss = self.step(criterion=criterion,
-                                      data=self.val_data)
+                test_loss = self.step(data=self.val_data)
 
             writer.add_scalars('loss', {'train': training_loss,
                                         'val': test_loss}, epoch + 1)
@@ -49,7 +46,7 @@ class Trainer:
         if self.args.save_weight:
             save_weights(self.model, self.args.model_path + self.model.name)
 
-    def step(self, criterion, data, optimizer=None):
+    def step(self, data, optimizer=None):
 
         data_loader = DataLoader(data, batch_size=self.args.batch_size, shuffle=False)
         total_loss = 0
@@ -63,8 +60,16 @@ class Trainer:
             y = torch.from_numpy(transform(y))
 
             outputs = self.model(x.permute(0, 3, 1, 2).float().to(self.device))
-            loss = criterion(outputs, y.long().to(self.device))
 
+            if self.loss_balance:
+                weight = np.bincount(y.reshape(-1)) + 1
+                weight = np.sum(weight) / weight
+                weight = torch.from_numpy(weight).float().to(self.device)
+                criterion = nn.CrossEntropyLoss(weight=weight)
+            else:
+                criterion = nn.CrossEntropyLoss()
+
+            loss = criterion(outputs, y.long().to(self.device))
             total_loss += (loss.item() * self.args.batch_size)
 
             if optimizer is not None:
@@ -72,10 +77,10 @@ class Trainer:
                 loss.backward()
                 optimizer.step()
 
-            if (step + 1) % 100 == 0:
+            if (step + 1) % self.args.batch_loss_print == 0:
                 print('step [{}/{}], Loss: {:.4f}, Perplexity: {:5.4f}'
                       .format(step + 1, num_of_step, loss.item(),
                               np.exp(loss.item())))
-
             step += 1
+
         return total_loss / (num_of_step * self.args.batch_size)
